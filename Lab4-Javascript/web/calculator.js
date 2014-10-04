24b978244e4a6a5755ab24fc273604b43f93bfc5
aguibert <andy.guibert@gmail.com>
2014-10-04 17:00:31 -0500

    Lab4 - working with closure and jQuery

--- Lab4-Javascript/web/calculator.js
+++ Lab4-Javascript/web/calculator.js
@@ -1,83 +1,63 @@
-function numClicked(num){
-	var scrn = document.getElementById("calcScreen");
-	if(scrn.value == undefined || scrn.value == ""){
-		if(num == '.')
-			setScreenVal(0.0);
-		else
-			setScreenVal(num);
-	}
-	else
-		scrn.value += num;
-	calc.lastNum = getScreenVal();
-	console.log("val=" + getScreenVal());
-}
-
-function operatorClicked(op){
-	if(op == '+') calc.curOp = calc.add;
-	else if(op == '-') calc.curOp = calc.sub;
-	else if(op == '*') calc.curOp = calc.mult;
-	else if(op == '/') calc.curOp = calc.div;
-	else console.log("ERROR: Operator not found: " + op);
-	if(calc.memoryValue == undefined)
-		calc.memoryValue = getScreenVal(); // store the old number in memory
-	calc.lastNum = undefined;
-	clearScreen();
-}
-
-function equalsClicked(){
-	calc.doOperation();
-}
-
-function clearScreen(){
-	document.getElementById("calcScreen").value = "";
-}
-
-function showMem(){
-	var scrn = document.getElementById("calcScreen");
-	if(calc.memoryValue == undefined)
-		scrn.value = "0";
-	else
+$(document).ready(function(){
+	var calc = {
+		curOp : undefined, // stores the current operation function to use
+		memoryValue : undefined, 
+		lastNum : undefined // for storing last number when equals is repeatedly pressed
+	};
+	
+	// Any number 0-9 or the '.' clicked
+	$(".num").click(function(){
+		var num = $(this).html();
+		var scrn = document.getElementById("calcScreen");
+		if(scrn.value == undefined || scrn.value == ""){
+			if(num == '.')
+				scrn.value = '0.';
+			else
+				setScreenVal(num);
+		} else {
+			scrn.value += num;
+		}
+		calc.lastNum = getScreenVal();
+	});
+	// Any operator clicked: + - * /
+	var operatorClicked =  function(){
+		calc.curOp = $(this).html();
+		if(calc.memoryValue == undefined)
+			calc.memoryValue = getScreenVal(); // store the old number in memory
+		calc.lastNum = undefined;
+		document.getElementById("calcScreen").value = "";
+	};
+	var equalsClicked = function(){
+		var newNum = (calc.lastNum == undefined) ? getScreenVal() : calc.lastNum;
+		calc.memoryValue = eval(calc.memoryValue + ' ' + calc.curOp + ' ' + newNum);
 		setScreenVal(calc.memoryValue);
-}
-
-function clearMem(){
-	console.log("mem cleared");
-	calc.memoryValue = undefined;
-	calc.lastNum = undefined;
-}
-
-function addMem(){
-	operatorClicked('+');
-	calc.lastNum = calc.memoryValue;
-	equalsClicked();
-}
+	};
+	$(".operator").click(operatorClicked);
+	$("#equals").click(equalsClicked);
+	$("#clearScreen").click(function(){
+		document.getElementById("calcScreen").value = "";
+	});
+	$("#showMem").click(function(){
+		var scrn = document.getElementById("calcScreen");
+		if(calc.memoryValue == undefined)
+			scrn.value = "0";
+		else
+			setScreenVal(calc.memoryValue);
+	});
+	$("#clearMem").click(function(){
+		calc.memoryValue = undefined;
+		calc.lastNum = undefined;
+	});
+	$("#addMem").click(function(){
+		document.getElementById("calcScreen").value = "";
+		calc.lastNum = calc.memoryValue;
+		equalsClicked();
+	});
+});
 
 function getScreenVal(){
 	return parseFloat(document.getElementById("calcScreen").value);
 }
 function setScreenVal(num){
-	document.getElementById("calcScreen").value = Math.round(num*10)/10; // clears up any ugly floating point imprecision
+	document.getElementById("calcScreen").value = Number(Number(num).toFixed(12)).toString();
 }
-
-var calc = {
-	memoryValue : undefined,
-	lastNum : undefined,
-	add : function(toAdd){
-		return this.memoryValue += toAdd;
-	},
-	sub : function(toSub){
-		return this.memoryValue -= toSub;
-	},
-	mult : function(toMult){
-		return this.memoryValue *= toMult;
-	},
-	div : function(toDiv){
-		return this.memoryValue /= toDiv;
-	},
-	doOperation : function(){
-		if(this.lastNum == undefined)
-			setScreenVal(this.curOp(getScreenVal()));
-		else
-			setScreenVal(this.curOp(this.lastNum));
-	}
-};
